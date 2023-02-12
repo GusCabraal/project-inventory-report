c0b3353badf9244c69ec51421f5b1d18ac0ce6a0
GusCabraal <gustavocabral97@hotmail.com>
2023-02-12 15:41:26 -0300

    fix: list error

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -5,26 +5,30 @@ from inventory_report.importer.xml_importer import XmlImporter
 import sys
 
 
+def generate_report(type_path, file_path, report_type):
+    if type_path == "csv":
+        inventory_instance = InventoryRefactor(CsvImporter)
+        sys.stdout.write(
+            inventory_instance.import_data(file_path, report_type)
+        )
+    if type_path == "json":
+        inventory_instance = InventoryRefactor(JsonImporter)
+        sys.stdout.write(
+            inventory_instance.import_data(file_path, report_type)
+        )
+    if type_path == "xml":
+        inventory_instance = InventoryRefactor(XmlImporter)
+        sys.stdout.write(
+            inventory_instance.import_data(file_path, report_type)
+        )
+
+
 def main():
     try:
         file_path = sys.argv[1]
         report_type = sys.argv[2]
         *rest, type_path = file_path.split(".")
-        if type_path == "csv":
-            inventory_instance = InventoryRefactor(CsvImporter)
-            sys.stdout.write(
-                inventory_instance.import_data(file_path, report_type)
-            )
-        if type_path == "json":
-            inventory_instance = InventoryRefactor(JsonImporter)
-            sys.stdout.write(
-                inventory_instance.import_data(file_path, report_type)
-            )
-        if type_path == "xml":
-            inventory_instance = InventoryRefactor(XmlImporter)
-            sys.stdout.write(
-                inventory_instance.import_data(file_path, report_type)
-            )
+        generate_report(type_path, file_path, report_type)
     except IndexError:
         sys.stderr.write("Verifique os argumentos\n")
 
