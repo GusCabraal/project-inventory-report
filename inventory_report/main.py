f1163966ff78e950672bc28887ad14b0ec810bc5
GusCabraal <gustavocabral97@hotmail.com>
2023-02-12 14:12:07 -0300

    feat: implements func main

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,32 @@
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+import sys
+
+
 def main():
-    pass
+    try:
+        file_path = sys.argv[1]
+        report_type = sys.argv[2]
+        *rest, type_path = file_path.split(".")
+        if type_path == "csv":
+            inventory_instance = InventoryRefactor(CsvImporter)
+            sys.stdout.write(
+                inventory_instance.import_data(file_path, report_type)
+            )
+        if type_path == "json":
+            inventory_instance = InventoryRefactor(JsonImporter)
+            sys.stdout.write(
+                inventory_instance.import_data(file_path, report_type)
+            )
+        if type_path == "xml":
+            inventory_instance = InventoryRefactor(XmlImporter)
+            sys.stdout.write(
+                inventory_instance.import_data(file_path, report_type)
+            )
+    except IndexError:
+        sys.stderr.write("Verifique os argumentos\n")
+
+
+main()
